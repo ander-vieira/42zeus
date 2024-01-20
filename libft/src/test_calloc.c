0a7b1ea98e1b7ac62641bd67b71dda1bb31634ee
ander-vieira <ander.vieira741@gmail.com>
2024-01-20 19:48:47 +0100

    Libft: remove ft_calloc tests for zero size (undef behavior)

--- libft/src/test_calloc.c
+++ libft/src/test_calloc.c
@@ -6,7 +6,7 @@
 /*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 12:25:29 by andeviei          #+#    #+#             */
-/*   Updated: 2024/01/20 18:39:20 by andeviei         ###   ########.fr       */
+/*   Updated: 2024/01/20 19:48:24 by andeviei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,9 @@ static void	test_calloc1(void)
 	tlib_print_test("2", tlib_alloc_count() == 1, FALSE);
 	tlib_print_test("3", tlib_alloc_lookup(buf) == 6, FALSE);
 	tlib_print_test("4", !memcmp(buf, "\0\0\0\0\0\0", 6), FALSE);
-	tlib_print_test("5", tlib_alloc_errors() == 0x00, FALSE);
+	tlib_print_test("5", tlib_alloc_errors() == 0x00, TRUE);
 	if (tlib_alloc_lookup(buf) != 0)
 		free(buf);
-	buf = ft_calloc(0, 2);
-	tlib_print_test("6", buf == NULL, FALSE);
-	tlib_print_test("7", tlib_alloc_count() == 0, FALSE);
-	tlib_print_test("8", tlib_alloc_errors() == ERRMALLOC_ZERO, TRUE);
-	tlib_alloc_reset();
 }
 
 void	test_calloc(void)
