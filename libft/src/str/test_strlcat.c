4fea88048372e56fd6fd697f1d52d4fd2f33afe0
andeviei <andeviei@c2r2s1.42urduliz.com>
2024-02-19 20:52:07 +0100

    libft: add tests for strlcpy and strlcat

--- libft/src/str/test_strlcat.c
+++ libft/src/str/test_strlcat.c
@@ -6,31 +6,59 @@
 /*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 13:22:09 by andeviei          #+#    #+#             */
-/*   Updated: 2024/02/17 17:15:42 by andeviei         ###   ########.fr       */
+/*   Updated: 2024/02/19 20:23:58 by andeviei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
 
-//TODO
+static void	test_strlcat_testone(char *str1, char *str2, char *expected)
+{
+	char	buf[5];
+
+	tlib_alloc_reset();
+	strcpy(buf, str1);
+	tlib_test_ok(ft_strlcat(buf, str2, 5) == strlen(str1) + strlen(str2));
+	tlib_test_ok(!strcmp(buf, expected));
+	tlib_test_ok(tlib_alloc_count() == 0);
+}
+
+static void	test_strlcat_child1(void)
+{
+	test_strlcat_testone("AB", "CD", "ABCD");
+	test_strlcat_testone("", "ABC", "ABC");
+	test_strlcat_testone("AB", "CDE", "ABCD");
+	test_strlcat_testone("", "ABCDE", "ABCD");
+}
+
+static void	test_strlcat_child2(void)
+{
+	char	buf[5];
+
+	tlib_alloc_reset();
+	strcpy(buf, "AB");
+	tlib_test_ok(ft_strlcat(buf, "C", 1) == 2);
+	tlib_test_ok(!strcmp(buf, "AB"));
+	strcpy(buf, "AB");
+	tlib_test_ok(ft_strlcat(buf, "C", 0) == 1);
+	tlib_test_ok(!strcmp(buf, "AB"));
+	tlib_test_ok(tlib_alloc_count() == 0);
+}
+
+static void	test_strlcat_child3(void)
+{
+	ft_strlcat(NULL, "HOLA", 5);
+}
+
+static void	test_strlcat_child4(void)
+{
+	ft_strlcat("HOLA", NULL, 5);
+}
 
 void	test_strlcat(void)
 {
-	char	buf[10];
-
-	strcpy(buf, "");
-	tlib_test_ok(ft_strlcat(buf, "HOLA", 10) == 4);
-	tlib_test_ok(!strcmp(buf, "HOLA"));
-	tlib_test_ok(ft_strlcat(buf, " ", 10) == 5);
-	tlib_test_ok(!strcmp(buf, "HOLA "));
-	tlib_test_ok(ft_strlcat(buf, "MUNDO", 10) == 10);
-	tlib_test_ok(!strcmp(buf, "HOLA MUND"));
-	strcpy(buf, "AB");
-	tlib_test_ok(ft_strlcat(buf, "CDE", 4) == 5);
-	tlib_test_ok(!strcmp(buf, "ABC"));
-	strcpy(buf, "ABC");
-	tlib_test_ok(ft_strlcat(buf, "DE", 2) == 4);
-	tlib_test_ok(!strcmp(buf, "ABC"));
-	tlib_test_ok(tlib_alloc_count() == 0);
-	tlib_alloc_reset();
+	tlib_test_process(&test_strlcat_child1, PRESULT_OK);
+	tlib_test_process(&test_strlcat_child2, PRESULT_OK);
+	tlib_test_process(&test_strlcat_child3, PRESULT_SEGFAULT);
+	tlib_test_process(&test_strlcat_child4, PRESULT_SEGFAULT);
 }
--- libft/src/str/test_strlcpy.c
+++ libft/src/str/test_strlcpy.c
@@ -6,29 +6,51 @@
 /*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 14:17:08 by andeviei          #+#    #+#             */
-/*   Updated: 2024/02/17 17:14:43 by andeviei         ###   ########.fr       */
+/*   Updated: 2024/02/19 20:21:13 by andeviei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
 
-//TODO
-
 static void	test_strlcpy_child1(void)
 {
-	char	buf[10];
+	char	buf[5];
 
 	tlib_alloc_reset();
-	tlib_test_ok(ft_strlcpy(buf, "ASDFG", 10) == 5);
-	tlib_test_ok(!strcmp(buf, "ASDFG"));
-	tlib_test_ok(ft_strlcpy(buf, "0123456789", 10) == 10);
-	tlib_test_ok(!strcmp(buf, "012345678"));
-	tlib_test_ok(ft_strlcpy(buf, "", 10) == 0);
+	tlib_test_ok(ft_strlcpy(buf, "", 5) == 0);
 	tlib_test_ok(!strcmp(buf, ""));
+	tlib_test_ok(ft_strlcpy(buf, "ABC", 5) == 3);
+	tlib_test_ok(!strcmp(buf, "ABC"));
+	tlib_test_ok(ft_strlcpy(buf, "ABCD", 5) == 4);
+	tlib_test_ok(!strcmp(buf, "ABCD"));
+	tlib_test_ok(ft_strlcpy(buf, "ABCDE", 5) == 5);
+	tlib_test_ok(!strcmp(buf, "ABCD"));
+	tlib_test_ok(ft_strlcpy(buf, "ABCDEFGH", 5) == 8);
+	tlib_test_ok(!strcmp(buf, "ABCD"));
+	tlib_test_ok(ft_strlcpy(buf, "", 0) == 0);
+	tlib_test_ok(buf[0] != '\0');
 	tlib_test_ok(tlib_alloc_count() == 0);
+}
+
+static void	test_strlcpy_child2(void)
+{
+	ft_strlcpy(NULL, "ABC", 5);
+}
+
+static void	test_strlcpy_child3(void)
+{
+	ft_strlcpy("ABC", NULL, 5);
+}
+
+static void	test_strlcpy_child4(void)
+{
+	ft_strlcpy(NULL, NULL, 5);
 }
 
 void	test_strlcpy(void)
 {
 	tlib_test_process(&test_strlcpy_child1, PRESULT_OK);
+	tlib_test_process(&test_strlcpy_child2, PRESULT_SEGFAULT);
+	tlib_test_process(&test_strlcpy_child3, PRESULT_SEGFAULT);
+	tlib_test_process(&test_strlcpy_child4, PRESULT_SEGFAULT);
 }
