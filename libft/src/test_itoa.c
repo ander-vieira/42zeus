e4068ca564231a19a99316b01154677949ba3239
ander-vieira <ander.vieira741@gmail.com>
2024-10-05 14:58:20 +0200

    Upgrade tests for itoa

--- libft/src/test_itoa.c
+++ libft/src/test_itoa.c
@@ -5,16 +5,16 @@ static void	test_itoa_testone(int num, char *expected) {
 
 	tlib_mockmalloc_reset();
 	str = ft_itoa(num);
-	tlib_testresult_raw(!strcmp(str, expected));
+	tlib_testresult_str(str, expected, "ft_itoa(%d)", num);
 	tlib_testmalloc_size(str, strlen(expected) + 1, "ft_itoa(%d)", num);
 	free(str);
 	tlib_testmalloc_leak("ft_itoa(%d)", num);
 }
 
 static void	test_itoa_child1(void) {
-	test_itoa_testone(314, "314");
+	test_itoa_testone(359, "359");
 	test_itoa_testone(0, "0");
-	test_itoa_testone(-4268, "-4268");
+	test_itoa_testone(-5096, "-5096");
 	test_itoa_testone(2147483647, "2147483647");
 	test_itoa_testone(-2147483648, "-2147483648");
 }
@@ -22,7 +22,7 @@ static void	test_itoa_child1(void) {
 static void	test_itoa_child2(void) {
 	tlib_mockmalloc_reset();
 	tlib_mockmalloc_setmock(1);
-	tlib_testresult_addr(ft_itoa(5), NULL, "ft_itoa(5) (with malloc fail)");
+	tlib_testresult_addr(ft_itoa(0), NULL, "ft_itoa(0) (with malloc fail)");
 }
 
 void	test_itoa(void) {
