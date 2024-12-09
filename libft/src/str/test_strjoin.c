f4dc2f5be7d27066db3534c259457cf6c32ef37d
ander-vieira <ander.vieira741@gmail.com>
2024-12-09 19:57:02 +0100

    libft: upgrade tests for strjoin

--- libft/src/str/test_strjoin.c
+++ libft/src/str/test_strjoin.c
@@ -22,14 +22,22 @@ static void	test_strjoin_child2(void) {
 	tlib_mockmalloc_reset();
 	tlib_mockmalloc_setmock(1);
 	tlib_testresult_addr(ft_strjoin("HOLA", " MUNDO"), NULL, "ft_strjoin(\"HOLA\", \" MUNDO\") (with malloc fail)");
+	tlib_testmalloc_leak("ft_strjoin(\"HOLA\", \" MUNDO\") (with malloc fail)");
 }
 
 static void	test_strjoin_child3(void) {
 	tlib_testresult_addr(ft_strjoin(NULL, " MUNDO"), NULL, "ft_strjoin(NULL, \" MUNDO\")");
+	tlib_testmalloc_leak("ft_strjoin(NULL, \" MUNDO\")");
 }
 
 static void	test_strjoin_child4(void) {
 	tlib_testresult_addr(ft_strjoin("HOLA", NULL), NULL, "ft_strjoin(\"HOLA\", NULL)");
+	tlib_testmalloc_leak("ft_strjoin(\"HOLA\", NULL)");
+}
+
+static void	test_strjoin_child5(void) {
+	tlib_testresult_addr(ft_strjoin(NULL, NULL), NULL, "ft_strjoin(NULL, NULL)");
+	tlib_testmalloc_leak("ft_strjoin(NULL, NULL)");
 }
 
 void	test_strjoin(void) {
@@ -37,4 +45,5 @@ void	test_strjoin(void) {
 	tlib_testprocess_ok(&test_strjoin_child2, "ft_strjoin(\"HOLA\", \" MUNDO\") (with malloc fail)");
 	tlib_testprocess_ok(&test_strjoin_child3, "ft_strjoin(NULL, \" MUNDO\")");
 	tlib_testprocess_ok(&test_strjoin_child4, "ft_strjoin(\"HOLA\", NULL)");
+	tlib_testprocess_ok(&test_strjoin_child5, "ft_strjoin(NULL, NULL)");
 }
